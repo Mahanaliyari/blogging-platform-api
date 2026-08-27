1cc0ca49f942f7ffa7460936634b3662b4eb8430
Mahan <164334687+Mahanaliyari@users.noreply.github.com>
2026-08-27 16:15:51 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -33,10 +33,7 @@ app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(likes.router)
 
-@app.get("/")
-def root():
-    return{'Hello World'}
-        
+
     
     
 
